73b59ed6e797a7a64c79dd67d64cf915e87d14ae
Jack McConnell <44751199+jwmcconnell@users.noreply.github.com>
2019-07-29 09:22:14 -0700

    Add chat with socket.io (#2)

--- public/src/workspace/WorkspaceApp.js
+++ public/src/workspace/WorkspaceApp.js
@@ -1,6 +1,8 @@
 import Component from '../Component.js';
 import { submitVerify } from '../services/auth-api.js';
 
+const socket = io();
+
 class WorkspaceApp extends Component {
 
   render() {
@@ -9,13 +11,35 @@ class WorkspaceApp extends Component {
     submitVerify()
       .then(res => console.log(res));
 
+    const form = dom.querySelector('.message-form');
+    const input = dom.querySelector('#message');
+    const messages = dom.querySelector('#messages');
+
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      socket.emit('chat message', input.value);
+      input.value = '';
+      return false;
+    });
+
+    socket.on('chat message', (msg) => {
+      const li = document.createElement('li');
+      li.textContent = msg;
+      messages.appendChild(li);
+    });
+    
+
     return dom;
   }
 
   renderTemplate() {
     return /*html*/`
       <div>
-          <h1>Workspace is working</h1>
+        <h1>Workspace is working</h1>
+        <ul id="messages"></ul>
+        <form class="message-form" action="">
+          <input id="message" autocomplete="off" /><button>Send</button>
+        </form>
       </div>
     `;
   }

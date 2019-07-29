ab25901d7b28481e817c2a4b0bdc9ed0b53f260c
Jack McConnell <jackmc1122@gmail.com>
2019-07-28 21:10:53 -0700

    Add chat with socket.io

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
 
-const app = require('./lib/app');
+const http = require('./lib/app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
 });

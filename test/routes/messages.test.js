cf62b4c6146ee6ffb710ae5d5281205a5eb2b5a1
Jackmc <jackmc1122@gmail.com>
2019-07-30 14:16:31 -0700

    Clean up messages test.

--- test/routes/messages.test.js
+++ test/routes/messages.test.js
@@ -1,23 +1,21 @@
-const { getToken, getUsers, getChannels } = require('../data-helpers');
+const { getToken, getChannels } = require('../data-helpers');
 const io = require('socket.io-client');
 
 describe('auth routes', () => {
   it('connects to a socket and sends a message', async(done) => {
     const token = getToken();
-    const user = getUsers()[0];
     const channel = getChannels()[0];
     const socket = io.connect('http://localhost:3000', {
       extraHeaders: { Cookie: token },
-      'reconnection delay' : 0
-      , 'reopen delay' : 0
-      , 'force new connection' : true
-      , transports: ['websocket']
+      'reconnection delay' : 0, 
+      'reopen delay' : 0, 
+      'force new connection' : true, 
+      transports: ['websocket']
     });
     socket.on('history', (msg) => {
       expect(msg).toEqual(expect.any(Array));
       done();
     });
     socket.emit('join', channel._id);
-    // socket.emit('chat message', { room: channel._id, message: 'Joining chat', user: user });
   });
 });

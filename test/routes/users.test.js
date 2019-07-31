9acf112a11b312f05ed3ecc9d4fe62058ce75a54
Jackmc <jackmc1122@gmail.com>
2019-07-31 10:36:42 -0700

    Add a users route to leave a workspace.

---
+++ test/routes/users.test.js
@@ -0,0 +1,19 @@
+const { getWorkspaces, getAgent, getUsers } = require('../data-helpers');
+const request = require('supertest');
+const app = require('../../lib/app');
+
+describe('auth routes', () => {
+  it('removes the relationship between user and workspace', () => {
+    const workspace = getWorkspaces()[0];
+    const user = getUsers()[0];
+    return getAgent()
+      .delete(`/api/v1/users/workspace/${workspace._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          user: user._id,
+          workspace: workspace._id
+        });
+      });
+  });
+});

6309871e75700f8a9e52d2b17f5cdd99f58d1b44
Jackmc <jackmc1122@gmail.com>
2019-08-01 09:38:38 -0700

    Increase the number of workspaces each seeded user creates.

--- test/seed-data.js
+++ test/seed-data.js
@@ -4,19 +4,21 @@ const Channel = require('../lib/models/Channel');
 const UserByWorkspace = require('../lib/models/UserByWorkspace');
 const chance = require('chance').Chance();
 
-module.exports = async({ users = 3 } = {}) => {
+module.exports = async({ users = 10, maxWorkspaces = 3 } = {}) => {
   const createdUsers = await User.create([...Array(users)].map(() => ({
     username: chance.name(),
     password: 'password'
   })));
 
   const createdWorkspaces = await Workspace.create(createdUsers.flatMap(user => {
-    return {
-      name: chance.animal(),
-      owner: user._id
-    };
+    return [...Array(chance.integer({ min: 1, max: maxWorkspaces }))]
+      .map(() => ({
+        name: chance.animal(),
+        owner: user._id
+      }));
   }));
 
+  // Create the member relationship between the owner and the workspace
   const createdUsersByWorkspaces = await UserByWorkspace
     .create(createdWorkspaces.map(workspace => {
       return { user: workspace.owner, workspace: workspace._id };

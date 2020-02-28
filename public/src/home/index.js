83fef36da3c92d8b230d4d53e3d2ac50cbe695db
Jackmc <jackmc1122@gmail.com>
2020-02-27 20:25:04 -0800

    Remove the public folder from the backend

--- public/src/home/index.js
+++
@@ -1,7 +0,0 @@
-import Home from './App.js';
-
-const home = new Home();
-const homeDOM = home.render();
-
-const root = document.getElementById('app');
-root.appendChild(homeDOM);
--- public/src/landing/index.js
+++
@@ -1,7 +0,0 @@
-import Landing from './LandingApp.js';
-
-const landing = new Landing();
-const landingDOM = landing.render();
-
-const root = document.getElementById('app');
-root.appendChild(landingDOM);

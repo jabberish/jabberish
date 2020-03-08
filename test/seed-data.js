ee0da1cbffd8e578465fe7a4f9faa9876bf8250e
Jackmc <jackmc1122@gmail.com>
2020-03-07 17:34:54 -0800

    Fix running build in CI

--- test/seed-data.js
+++ test/seed-data.js
@@ -5,7 +5,7 @@ const Message = require('../lib/models/Message');
 const UserByWorkspace = require('../lib/models/UserByWorkspace');
 const chance = require('chance').Chance();
 
-module.exports = async ({ users = 5, maxWorkspaces = 3, maxChannels = 3, maxMessages = 20 } = {}) => {
+module.exports = async ({ users = 5, maxWorkspaces = 3, maxChannels = 3, maxMessages = 10 } = {}) => {
   const createdUsers = await User.create(
     [...Array(users)].map(() => ({
       username: chance.name(),
